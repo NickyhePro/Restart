1f3517eb5ac24a636c8ef4a81741df42fc6586fe
Nick He <nickyhe123@gmail.com>
2022-02-12 11:27:49 +1300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Nick He on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
