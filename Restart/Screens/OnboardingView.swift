07b68700c9e1736f76e6818ee81e265346f42ce5
Nick He <nickyhe123@gmail.com>
2022-02-12 17:01:01 +1300

    Implement the UI of the OnBoardingView screen

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -16,14 +16,102 @@ struct OnboardingView: View {
     // MARK: - BODY
     
     var body: some View {
-        VStack {
-            Text("Onboarding!")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue").ignoresSafeArea(.all, edges: .all)
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(10)
+                    
+                    
+                }
+                
+                // MARK: - CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+
+                }
+                
+                Spacer()
+                
+                // MARK: - FOOTER
+                
+                ZStack {
+                    // 1. Circle static
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2)).padding(8)
+                    
+                    // 2. Call-to-action
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. Capsule dynamic width
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // 4. Draggable circle
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .foregroundColor(.white)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                .frame(height: 80, alignment: .center)
+                .padding()
+            
             }
         }
     }
