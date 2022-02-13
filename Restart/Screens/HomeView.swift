d6a2764386a8c29d44030603fcc083a8b2800ad9
Nick He <nickyhe123@gmail.com>
2022-02-13 14:48:31 +1300

    Implement the UI for HomeView and create a reusable CircleGroup UI component

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -16,15 +16,48 @@ struct HomeView: View {
     // MARK: - BODY
     
     var body: some View {
-        VStack {
-            Text("Home!")
-                .font(.largeTitle)
+        VStack(spacing: 20) {
+            // MARK: - Header
+            
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .yellow, ShapeOpacity: 0.2)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            }
+            
+            // MARK: - Center
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding(25)
+               
+            
+             
+            // MARK: - Footer
+            
+            Spacer()
             
             Button(action: {
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill").imageScale(.large)
+                
                 Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            
         }
     }
 }
